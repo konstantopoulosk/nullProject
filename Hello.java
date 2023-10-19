7adab630e3517d845292ecf1e1e4eb04275989c7
Kostas <t8220074@aueb.gr>
2023-10-19 11:55:01 +0300

    Eftiaja koumpi, anti gia to hello

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
-public class Hello{
+import javax.swing.*;
+class gui{
     public static void main(String args[]){
-        System.out.println("Hello");
+       JFrame frame = new JFrame("Button");
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setSize(400,300);
+       JButton button = new JButton("Press");
+       frame.getContentPane().add(button);
+       frame.getContentPane().add(button); // Adds Button to content pane of frame
+       frame.setVisible(true);
     }
 }
